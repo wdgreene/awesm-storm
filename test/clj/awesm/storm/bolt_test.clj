47955c3d09dc08fd2cf79e1e5dbc2e754928c902
William Greene <william.greene@gmail.com>
2012-02-14 12:12:56 -0800

    added a geo bolt test
    - added another botbolt test

--- test/clj/awesm/storm/bolt_test.clj
+++ test/clj/awesm/storm/bolt_test.clj
@@ -1,7 +1,7 @@
 (ns awesm.storm.bolt-test
   (:use [clojure test])
   (:import [awesm.storm.spout RandomClickEventSpout])
-  (:import [awesm.storm.bolt BotFlagBolt ReferrerBolt])
+  (:import [awesm.storm.bolt BotFlagBolt ReferrerBolt GeoBolt])
   (:use [backtype.storm bootstrap testing])
   (:use [backtype.storm.daemon common])
   )
@@ -19,13 +19,16 @@
           results (complete-topology cluster
               topology
               :mock-sources {"1" [["{'http_user_agent': 'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1)', 'bot_flag': 0}"]
+                                  ["{'http_user_agent': 'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1)', 'bot_flag': 1}"]
                                   ["{'http_user_agent': 'AppEngine-Google', 'bot_flag': 0}"]]}
               :storm-conf {TOPOLOGY-DEBUG true
                                 TOPOLOGY-WORKERS 2})]
       (is (ms= [["{'http_user_agent': 'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1)', 'bot_flag': 0}"]
+                ["{'http_user_agent': 'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1)', 'bot_flag': 1}"]
                 ["{'http_user_agent': 'AppEngine-Google', 'bot_flag': 0}"]]
             (read-tuples results "1")))
       (is (ms= [["{\"http_user_agent\":\"Mozilla\\/5.0 (compatible; MSIE 6.0; Windows NT 5.1)\",\"bot_flag\":0,\"new_bot_flag\":0}"]
+                ["{\"http_user_agent\":\"Mozilla\\/5.0 (compatible; MSIE 6.0; Windows NT 5.1)\",\"bot_flag\":1,\"new_bot_flag\":1}"]
                 ["{\"http_user_agent\":\"AppEngine-Google\",\"bot_flag\":0,\"new_bot_flag\":1}"]]
             (read-tuples results "2")))
       ))))
@@ -46,5 +49,27 @@
         (is (ms= [["{'referrer': 'http://test.awe.sm/zomg.php?query=val#anchor'}"]]
               (read-tuples results "1")))
         (is (ms= [["{\"referrer\":\"http://test.awe.sm/zomg.php?query=val#anchor\",\"referrer_domain\":\"http://test.awe.sm\"}"]]
+              (read-tuples results "2")))
+        ))))
+
+(deftest test-geobolt
+  (doseq [zmq-on? [false false]]
+    (with-simulated-time-local-cluster [cluster :supervisors 4
+                                        :daemon-conf {STORM-LOCAL-MODE-ZMQ zmq-on?}]
+      (let [topology (thrift/mk-topology
+        {"1" (thrift/mk-spout-spec (RandomClickEventSpout.))}
+        {"2" (thrift/mk-bolt-spec {"1" :shuffle} (GeoBolt.) :parallelism-hint 1)
+         })
+            results (complete-topology cluster
+          topology
+          :mock-sources {"1" [["{'remote_addr': '151.38.39.114'}"]
+                              ["{'remote_addr': '64.81.104.131'}"]]}
+          :storm-conf {TOPOLOGY-DEBUG true
+                       TOPOLOGY-WORKERS 2})]
+        (is (ms= [["{'remote_addr': '151.38.39.114'}"]
+                  ["{'remote_addr': '64.81.104.131'}"]]
+              (read-tuples results "1")))
+        (is (ms= [["{\"remote_addr\":\"151.38.39.114\",\"geo_country_code\":\"IT\",\"geo_country\":\"Italy\",\"geo_region\":null,\"geo_city\":null}"]
+                  ["{\"remote_addr\":\"64.81.104.131\",\"geo_country_code\":\"US\",\"geo_country\":\"United States\",\"geo_region\":\"CA\",\"geo_city\":\"Los Angeles\"}"]]
               (read-tuples results "2")))
         ))))
